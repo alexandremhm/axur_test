d842f6c853d51900181e7199e753be424e8aad2d
Matheus Alexandre <alexandremhm@gmail.com>
2022-05-31 19:32:15 -0300

    Revert "csv-reader-function"

--- server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { app } = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`Server open on ${PORT} PORT`));

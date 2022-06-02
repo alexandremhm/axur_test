547ef38d901f54752e63d4a496ce66ef082ab6b1
Matheus Alexandre <alexandremhm@gmail.com>
2022-06-01 22:59:41 -0300

    async lib for throttle the request rate nodejs install and integration

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const async = require('async');
 const { readCSV } = require('./src/helpers/read-csv');
 const { formatContactList } = require('./src/helpers/format-contact');
 const { createContact } = require('./src/api/external-requests/create-contact');
 const { getAllContacts } = require('./src/api/external-requests/get-all-contacts');
 const { addContactsToList } = require('./src/api/external-requests/insert-contacts-to-list');
 const { createList } = require('./src/api/external-requests/create-list');
-// const routes = require('./src/api/routes');
+const routes = require('./src/api/routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// app.use('/axur', routes.contact);
+app.use('/axur', routes.contact);
 
 const csvFilePath = '/home/matheus-alexandre/desafios_tecnicos/axur_test/src/Contatos.csv';
 
@@ -22,8 +23,8 @@ createList().then((response) => response);
 readCSV(csvFilePath)
   .then((response) => formatContactList(response))
   .then((response) => {
-    response.forEach(async (contact) => {
-      await createContact(contact);
+    response.forEach((contact) => {
+      async.queue(() => createContact(contact), 1);
     });
   });
 
